2b6d54dd1de6f6be89c0c35064c220a1c1dc2fc3
Lama Sandeep <lamasandeep5@gmail.com>
2019-02-12 13:51:25 +0545

    scrolling to top page button addes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 
 
-const openDoor = document.querySelector(".enter");
 
-openDoor.addEventListener('animationstart', function (event) {
-  console.log('Animation started', event);
-})
-
-openDoor.addEventListener('animationend', function (event) {
-  console.log('Animation ended', event);
-})
-
-openDoor.addEventListener('animationiteration', function (event) {
-  console.log('Animation iteration', event);
-})
 
 
 var modal = document.getElementById('contact');
